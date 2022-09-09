796caefacc3ceb37bd6a6195469a88b9ec15dd40
GUGALU <gugalupolli@gmail.com>
2022-09-08 23:55:02 -0300

    inicio parametros

---
+++ Sexto Codigo/Sexto Codigo/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sexto_Codigo
+{
+    internal class Calculadora
+    {
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+    }
+}
---
+++ Sexto Codigo/Sexto Codigo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sexto_Codigo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma1 = Calculadora.Sum(1, 3);
+            int soma2 = Calculadora.Sum(1, 2, 5);
+            int soma3 = Calculadora.Sum(1, 5, 6, 7);
+
+            Console.WriteLine(soma1);
+            Console.WriteLine(soma2);
+            Console.WriteLine(soma3);
+        }
+    }
+}
