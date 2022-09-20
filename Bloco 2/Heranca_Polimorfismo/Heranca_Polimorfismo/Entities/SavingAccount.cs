a007c5ee9454836c8e86eefddf7f1b344990d0bf
GUGALU <gugalupolli@gmail.com>
2022-09-20 09:49:44 -0300

    Upcasting e Downcasting

---
+++ Bloco 2/Heranca_Polimorfismo/Heranca_Polimorfismo/Entities/SavingAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca_Polimorfismo.Entities
+{
+    internal class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestrate) : base(number, holder, balance)
+        {
+            InterestRate = interestrate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- Bloco 2/Heranca_Polimorfismo/Heranca_Polimorfismo/Program.cs
+++ Bloco 2/Heranca_Polimorfismo/Heranca_Polimorfismo/Program.cs
@@ -11,12 +11,36 @@ namespace Heranca_Polimorfismo
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(1500, "Gustavo", 5950.00, 2500.00);
+            Account account = new Account(1000, "Fernanda", 2050.0);
+            BusinessAccount businessAccount = new BusinessAccount(1100, "Gustavo", 5950.00, 2500.00);
 
-            Console.WriteLine(account.Balance);
+            //UPCASTING
 
-            //account.Balance = 300.00;
-            // ^^ não e posivel ja que adicionei protected ao set, apenas poderei modificar em sub classes
+            Account account1 = businessAccount;
+            Account account2 = new BusinessAccount(1200, "Julio", 1350.0, 650.0);
+            Account account3 = new SavingAccount(1300, "Maria", 5000.0, 0.01);
+
+            //DOWNCASTING(inseguro)
+
+            BusinessAccount account4 = (BusinessAccount)account2;
+            account4.Loan(100.0);
+
+            if(account3 is BusinessAccount)
+            {
+                //BusinessAccount account5 = (BusinessAccount)account3;
+                BusinessAccount account5 = account3 as BusinessAccount;
+                account5.Loan(120.0);
+                Console.WriteLine("Loan");
+            }
+
+            if(account3 is SavingAccount)
+            {
+                //SavingAccount account5 = (SavingAccount)account3;
+                SavingAccount account5 = account3 as SavingAccount;
+                account5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
+
         }
     }
 }
