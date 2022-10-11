0cd8bffb827385f1cae164951e7decc4668dc8cf
GUGALU <gugalupolli@gmail.com>
2022-10-11 19:53:29 -0300

    File Info

---
+++ Bloco 2/FilesInfo/FilesInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FilesInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\FilesInfo\\File.txt";
+
+            string targetPath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\FilesInfo\\File2.txt";
+
+            if (File.Exists(targetPath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\FilesInfo\\File2.txt"))
+            {
+                File.Delete(targetPath);
+            }
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException erro)
+            {
+                Console.WriteLine("An erro ocurred");
+                Console.WriteLine(erro.Message);
+            }
+        }
+    }
+}
