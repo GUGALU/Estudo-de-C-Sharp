eec6bcca2fc20bd9fae715bf3b02ac3e7f4c7a2a
GUGALU <gugalupolli@gmail.com>
2022-10-11 20:31:25 -0300

    File Stream

--- Bloco 2/FilesInfo/FilesInfo/Program.cs
+++ Bloco 2/FilesInfo/FilesInfo/Program.cs
@@ -31,6 +31,29 @@ namespace FilesInfo
                 Console.WriteLine("An erro ocurred");
                 Console.WriteLine(erro.Message);
             }
+
+            string path = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\FilesInfo\\FileStream.txt";
+
+            FileStream fileStream = null;
+            StreamReader streamReader = null;
+
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open);
+                streamReader = new StreamReader(fileStream);
+                string line = streamReader.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch(IOException erro)
+            {
+                Console.WriteLine("An erro ocurred");
+                Console.WriteLine(erro.Message);
+            }
+            finally
+            {
+                if(fileStream != null) fileStream.Close();
+                if(streamReader != null) streamReader.Close();
+            }
         }
     }
 }
