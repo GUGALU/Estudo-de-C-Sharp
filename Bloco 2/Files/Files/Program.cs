dae3bd4b643e87fc821f77e26cf0da7cbc4c47a1
GUGALU <gugalupolli@gmail.com>
2022-10-18 11:38:50 -0300

    Directory

--- Bloco 2/Files/Files/Program.cs
+++ Bloco 2/Files/Files/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Files
 {
@@ -11,25 +9,51 @@ namespace Files
     {
         static void Main(string[] args)
         {
-            string sourcePath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text.txt";
-            string targetPath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text2.txt";
-            if(targetPath != @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text2.txt")
-            {
-                File.Create(targetPath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text2.txt");
-            }
+            //string sourcePath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text.txt";
+            //string targetPath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text2.txt";
+            //if (targetPath != @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text2.txt")
+            //{
+            //    File.Create(targetPath = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\text2.txt");
+            //}
+
+            //try
+            //{
+            //    string[] lines = File.ReadAllLines(sourcePath);
+
+            //    using (StreamWriter streamWriter = File.AppendText(targetPath))
+            //    {
+            //        foreach (string line in lines)
+            //        {
+            //            streamWriter.WriteLine(line.ToUpper());
+            //        }
+            //    }
+
+            //}
+            //catch (IOException err)
+            //{
+            //    Console.WriteLine("Error");
+            //    Console.WriteLine(err.Message);
+            //}
+
+            string path = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\Files\\Folder";
 
             try
             {
-                string[] lines = File.ReadAllLines(sourcePath);
-
-                using(StreamWriter streamWriter = File.AppendText(targetPath))
+                var folders =  Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders: ");
+                foreach(string s in folders)
                 {
-                    foreach(string line in lines)
-                    {
-                        streamWriter.WriteLine(line.ToUpper());
-                    }
+                    Console.WriteLine(s);
                 }
 
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + "\\newFolder");
             }
             catch (IOException err)
             {
