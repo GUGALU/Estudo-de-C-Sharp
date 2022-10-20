c4964c4726887c2c5584c8d75e651896522530f8
GUGALU <gugalupolli@gmail.com>
2022-10-20 10:17:58 -0300

    Generics inicio

---
+++ Bloco 2/Generics1/Generics1/PrintService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Generics1
+{
+    class PrintService
+    {
+
+        private int[] _values = new int[10]; //max 10 valores
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ Bloco 2/Generics1/Generics1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
