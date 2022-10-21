32830bab745c61064a8db37f6ebd1639d3196de4
GUGALU <gugalupolli@gmail.com>
2022-10-21 09:26:44 -0300

    HashSet e SortedSet

---
+++ Bloco 2/HashSet e SortedSet/HashSet e SortedSet/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSet_e_SortedSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (String p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            //união
+            Console.Write("União: ");
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            printCollection(c);
+
+            //intersecsão
+            Console.Write("Intersecsão: ");
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            printCollection(d);
+
+            //diferenciação
+            Console.Write("Diferenciação: ");
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            printCollection(e);
+
+        }
+        static void printCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
