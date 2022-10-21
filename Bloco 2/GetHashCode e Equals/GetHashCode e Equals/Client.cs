72608bbbfa78ec4d920e427c0476f18b9b9382e9
GUGALU <gugalupolli@gmail.com>
2022-10-21 09:13:29 -0300

    GetHashCode e Equals

---
+++ Bloco 2/GetHashCode e Equals/GetHashCode e Equals/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHashCode_e_Equals
+{
+    internal class Client
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Client))
+            {
+                return false;
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+
+
+    }
+}
---
+++ Bloco 2/GetHashCode e Equals/GetHashCode e Equals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GetHashCode_e_Equals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Julia", Email = "julia@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); //comparar conteudo
+            Console.WriteLine(a == b); //comparar memoria
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
