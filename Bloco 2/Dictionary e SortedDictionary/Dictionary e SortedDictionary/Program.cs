7edebb2bf13fb4619f17f1c66482a573acf0ad14
GUGALU <gugalupolli@gmail.com>
2022-10-31 09:08:35 -0300

    Dictionary e SortedDictionary

---
+++ Bloco 2/Dictionary e SortedDictionary/Dictionary e SortedDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_e_SortedDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99771122";
+            cookies["phone"] = "99771133";
+
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email");
+            Console.WriteLine("Phone number: " + cookies["phone"]);
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine("Email: " + cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is not 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.WriteLine("ALL COOKIES:");
+
+            foreach (var item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
