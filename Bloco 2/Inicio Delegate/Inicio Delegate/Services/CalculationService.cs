e95664dc75eb56f627ce0d935d3ea686403dbb31
GUGALU <gugalupolli@gmail.com>
2022-10-31 06:37:21 -0300

    Multicast Delegate

--- Bloco 2/Inicio Delegate/Inicio Delegate/Services/CalculationService.cs
+++ Bloco 2/Inicio Delegate/Inicio Delegate/Services/CalculationService.cs
@@ -8,17 +8,15 @@ namespace Inicio_Delegate.Services
 {
     class CalculationService
     {
-        public static double Max(double x, double y)
+        public static void ShowMax(double x, double y)
         {
-            return (x > y) ? x : y;
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
         }
-        public static double Sum(double x, double y)
+        public static void ShowSum(double x, double y)
         {
-            return x + y;
-        }
-        public static double Square(double x)
-        {
-            return x * x;
+            double sum = x + y;
+            Console.WriteLine(sum);
         }
     }
 }
