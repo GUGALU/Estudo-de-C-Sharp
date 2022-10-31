26e0acf50063cf9fde176cd01518dfe84448d2b4
GUGALU <gugalupolli@gmail.com>
2022-10-31 06:28:28 -0300

    Inicio Delegate

---
+++ Bloco 2/Inicio Delegate/Inicio Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using Inicio_Delegate.Services;
+using System;
+
+namespace Inicio_Delegate
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            // BinaryNumericOperation op = CalculationService.Sum; deste modo so pode usar quem casa com a asinatura da função
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Bloco 2/Inicio Delegate/Inicio Delegate/Services/CalculationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicio_Delegate.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
