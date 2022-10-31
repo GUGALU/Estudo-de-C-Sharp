7049c229396bb9f9c3c8de8c6e89ccb0068973f3
GUGALU <gugalupolli@gmail.com>
2022-10-31 06:27:59 -0300

    Comparison

---
+++ Bloco 2/Comparison/Comparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Comparison.Entities;
+
+namespace Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
