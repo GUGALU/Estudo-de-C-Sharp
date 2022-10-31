d98e9523f44200065af26af198a0331a7356fcbb
GUGALU <gugalupolli@gmail.com>
2022-10-31 08:32:11 -0300

    Linq Lambda

---
+++ Bloco 2/LinQ Lambda/LinQ Lambda/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LinQ_Lambda.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ Bloco 2/LinQ Lambda/LinQ Lambda/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+
+namespace LinQ_Lambda.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;
+        }
+    }
+}
