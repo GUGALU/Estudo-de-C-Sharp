b150551a6a39d963db5bd11e054af652a0f4cbb4
GUGALU <gugalupolli@gmail.com>
2022-10-31 08:31:26 -0300

    Inicio LinQ

---
+++ Bloco 2/LinQ Inicio/LinQ Inicio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_Inicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            IEnumerable<int> results = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            foreach(int x in results)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
