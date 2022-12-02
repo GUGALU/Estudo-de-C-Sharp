11d0ebddc0b7c52bbb1ef94ac27575019e55c9ea
GUGALU <gugalupolli@gmail.com>
2022-12-01 22:31:04 -0300

    Update

---
+++ Bloco 2/SalesWebMvc/SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Utilits" });
+
+            return View(list);
+        }
+    }
+}
--- Bloco 2/using_block/using_block/Program.cs
+++ Bloco 2/using_block/using_block/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace using_block
 {
@@ -10,6 +11,25 @@ namespace using_block
     {
         static void Main(string[] args)
         {
+
+            string path = @"C:\\Users\\Gustavo\\Desktop\\GU\\Code\\C#\\Bloco 2\\using_block\\text.txt";
+
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string line = streamReader.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException err)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(err.Message);
+            }
         }
     }
 }
